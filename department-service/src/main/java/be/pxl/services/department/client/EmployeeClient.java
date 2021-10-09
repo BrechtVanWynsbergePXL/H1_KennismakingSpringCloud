20e1122fa8b7a498f43aa98736c06561959e243d
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2021-10-09 14:21:52 +0200

    H1_Kennismaking Spring Cloud afgewerkt

--- department-service/src/main/java/be/pxl/services/department/client/EmployeeClient.java
+++ department-service/src/main/java/be/pxl/services/department/client/EmployeeClient.java
@@ -1,6 +1,13 @@
 package be.pxl.services.department.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "employee-service")
 public interface EmployeeClient {
-
-
+    @GetMapping("/organization/{organizationId}")
+    List findByOrganization(@PathVariable("organizationId") Long organizationId);
 }
--- organization-service/src/main/java/be/pxl/services/organization/OrganizationApplication.java
+++ organization-service/src/main/java/be/pxl/services/organization/OrganizationApplication.java
@@ -1,27 +1,91 @@
 package be.pxl.services.organization;
 
+import be.pxl.services.organization.client.DepartmentClient;
+import be.pxl.services.organization.client.EmployeeClient;
+import be.pxl.services.organization.controller.OrganizationController;
+import be.pxl.services.organization.model.Organization;
+import be.pxl.services.organization.repository.OrganizationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+//@EnableSwagger2
 public class OrganizationApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrganizationController.class);
+
+	@Autowired
+	OrganizationRepository repository;
+	@Autowired
+	DepartmentClient departmentClient;
+	@Autowired
+	EmployeeClient employeeClient;
 
 	public static void main(String[] args) {
-		//SpringApplication.run(OrganizationApplication.class, args);
+		SpringApplication.run(OrganizationApplication.class, args);
 	}
-//
-//	@Bean
-//	public Docket swaggerApi() {
-//		return new Docket(DocumentationType.SWAGGER_2)
-//				.select()
-//					.apis(RequestHandlerSelectors.basePackage("be.pxl.services.organization.controller"))
-//					.paths(PathSelectors.any())
-//				.build()
-//				.apiInfo(new ApiInfoBuilder().version("1.0").title("Organization API").description("Documentation Organization API v1.0").build());
-//	}
-//
-//	@Bean
-//    OrganizationRepository repository() {
-//		OrganizationRepository repository = new OrganizationRepository();
-//		repository.add(new Organization("Microsoft", "Redmond, Washington, USA"));
-//		repository.add(new Organization("Oracle", "Redwood City, California, USA"));
-//		return repository;
-//	}
-	
+
+	@Bean
+	public Docket swaggerApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+					.apis(RequestHandlerSelectors.basePackage("be.pxl.services.organization.controller"))
+					.paths(PathSelectors.any())
+				.build()
+				.apiInfo(new ApiInfoBuilder().version("1.0").title("Organization API").description("Documentation Organization API v1.0").build());
+	}
+
+	@Bean
+    OrganizationRepository repository() {
+		OrganizationRepository repository = new OrganizationRepository();
+		repository.add(new Organization("Microsoft", "Redmond, Washington, USA"));
+		repository.add(new Organization("Oracle", "Redwood City, California, USA"));
+		return repository;
+	}
+
+	@GetMapping("/{id}")
+	public Organization findById(@PathVariable("id") Long id) {
+		LOGGER.info("Organization find: id={}", id);
+		return repository.findById(id);
+	}
+
+	@GetMapping("/{id}/with-departments")
+	public Organization findByIdWithDepartments(@PathVariable("id") Long id) {
+		LOGGER.info("Organization find: id={}", id);
+		Organization organization = repository.findById(id);
+		organization.setDepartments(departmentClient.findByOrganization(organization.getId()));
+		return organization;
+	}
+
+	@GetMapping("/{id}/with-departments-and-employees")
+	public Organization findByIdWithDepartmentsAndEmployees(@PathVariable("id") Long id) {
+		LOGGER.info("Organization find: id={}", id);
+		Organization organization = repository.findById(id);
+		organization.setDepartments(departmentClient.findByOrganizationWithEmployees(organization.getId()));
+		return organization;
+	}
+
+	@GetMapping("/{id}/with-employees")
+	public Organization findByIdWithEmployees(@PathVariable("id") Long id) {
+		LOGGER.info("Organization find: id={}", id);
+		Organization organization = repository.findById(id);
+		organization.setEmployees(employeeClient.findByOrganization(organization.getId()));
+		return organization;
+	}
+
 }
